daba286fc79a359268ac82cfcf8bbb678a114109
clauvessouza <claudio.alves@estudante.iftm.edu.br>
2024-07-15 22:23:57 -0300

    exercicio treino

---
+++ exe1080.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class exe1080{
+	public static void main(String args[]) {
+		Scanner s = new Scanner(System.in);
+		int n = 0 ; //recebe valores a serem analisados;
+		int maior = 0; // armazena qual o maior valor;
+		int posicao = 0; // armazena qual a posição o valor esta localizado;
+		
+		for (int i = 0; i <100; i++) {
+			n = s.nextInt();
+			if(n>maior){
+			 maior = n;
+			 posicao = i+1;
+			}
+			
+		}
+		
+		System.out.println(maior);
+		System.out.println(posicao);
+	}
+}
